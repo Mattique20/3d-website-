d9a764e821ae13271839e227bba8a2f683457066
Mohammad Attique <mattique02@outlook.com>
2025-02-19 23:06:35 +0500

    Update Roadmodel.jsx

--- portfolio/src/Roadmodel.jsx
+++ portfolio/src/Roadmodel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useGLTF } from "@react-three/drei";
 
 const RoadModel = ({ position = [0, 0, 0] }) => {
-    const { scene } = useGLTF("./public/street_scene/scene.gltf"); 
+    const { scene } = useGLTF("/street_scene/scene.gltf"); 
     const clonedScene = scene.clone(); // Clone the scene for independent instances
 
     return <primitive object={clonedScene} position={position} scale={1} />;
